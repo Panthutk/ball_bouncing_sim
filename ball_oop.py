6dca5e151852228017c041d54db29a8c9b27dc07
Panthut Ketphan <panthat.k@gmail.com>
2023-11-19 15:26:22 +0700

    ball but it keeping forming line of ball

--- ball_oop.py
+++ ball_oop.py
@@ -5,6 +5,11 @@ import random
 class Ball:
     def __init__(self):
         self.turtle = turtle.Turtle()
+        self.xpos = []
+        self.ypos = []
+        self.vx = []
+        self.vy = []
+        self.ball_color = []
 
     def draw_circle(self, color, size, x, y):
         self.turtle.penup()
@@ -17,42 +22,48 @@ class Ball:
         self.turtle.end_fill()
         self.turtle.hideturtle()
 
-    def move_circle(self, i, xpos, ypos, vx, vy, canvas_width, canvas_height, ball_radius):
-        self.xpos = xpos
-        self.ypos = ypos
-        self.vx = vx
-        self.vy = vy
-        self.canvas_width = canvas_width
-        self.canvas_height = canvas_height
-        self.ball_radius = ball_radius
+    def move_circle(self, i, canvas_width, canvas_height, ball_radius):
         self.xpos[i] += self.vx[i]
         self.ypos[i] += self.vy[i]
 
-        if abs(self.xpos[i] + self.vx[i]) > (self.canvas_width - self.ball_radius):
+        # Reflect from walls
+        if abs(self.xpos[i] + self.vx[i]) > (canvas_width - ball_radius):
             self.vx[i] = -self.vx[i]
 
-        if abs(self.ypos[i] + self.vy[i]) > (self.canvas_height - self.ball_radius):
+        if abs(self.ypos[i] + self.vy[i]) > (canvas_height - ball_radius):
             self.vy[i] = -self.vy[i]
 
-    def initilizing(self, xpos, ypos, vx, vy, ball_color, canvas_width, canvas_height, ball_radius, num_balls):
-        self.xpos = xpos
-        self.ypos = ypos
-        self.vx = vx
-        self.vy = vy
-        self.ball_color = ball_color
+        # Check for collisions with other balls
+        for j in range(len(self.xpos)):
+            if i != j:
+                distance = ((self.xpos[i] - self.xpos[j]) ** 2 +
+                            (self.ypos[i] - self.ypos[j]) ** 2) ** 0.5
+                min_distance = 2 * ball_radius
+                if distance < min_distance:
+                    # If collision, adjust ball position randomly
+                    self.xpos[i] = random.randint(
+                        -1 * canvas_width + ball_radius, canvas_width - ball_radius)
+                    self.ypos[i] = random.randint(
+                        -1 * canvas_height + ball_radius, canvas_height - ball_radius)
+
+        # Reflect from walls
+        if abs(self.xpos[i] + self.vx[i]) > (canvas_width - ball_radius):
+            self.vx[i] = -self.vx[i]
+
+        if abs(self.ypos[i] + self.vy[i]) > (canvas_height - ball_radius):
+            self.vy[i] = -self.vy[i]
+
+    def initializing(self, canvas_width, canvas_height, ball_radius, num_balls):
         self.canvas_width = canvas_width
         self.canvas_height = canvas_height
         self.ball_radius = ball_radius
         self.num_balls = num_balls
         for i in range(self.num_balls):
-            self.xpos.append(random.randint(-1*self.canvas_width +
-                                            self.ball_radius, self.canvas_width - self.ball_radius))
-            self.ypos.append(random.randint(-1*self.canvas_height +
-                                            self.ball_radius, self.canvas_height - self.ball_radius))
-            self.vx.append(random.randint(1, 0.01*self.canvas_width))
-            self.vy.append(random.randint(1, 0.01*self.canvas_height))
+            self.xpos.append(random.randint(
+                -1 * self.canvas_width + self.ball_radius, self.canvas_width - self.ball_radius))
+            self.ypos.append(random.randint(
+                -1 * self.canvas_height + self.ball_radius, self.canvas_height - self.ball_radius))
+            self.vx.append(random.randint(1, 10))
+            self.vy.append(random.randint(1, 10))
             self.ball_color.append(
                 (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-
-
-my_ball = Ball()
--- run_ball.py
+++ run_ball.py
@@ -1,7 +1,11 @@
 import turtle
-import ball
+import random
+from ball_oop import Ball
 
-num_balls = int(input("Number of balls to simulate: "))
+balls = Ball()
+
+balls = Ball()
+
 turtle.speed(0)
 turtle.tracer(0)
 turtle.hideturtle()
@@ -9,21 +13,20 @@ canvas_width = turtle.screensize()[0]
 canvas_height = turtle.screensize()[1]
 ball_radius = 0.05 * canvas_width
 turtle.colormode(255)
-color_list = []
-xpos = []
-ypos = []
-vx = []
-vy = []
-ball_color = []
-ball.initilizing(xpos, ypos, vx, vy, ball_color, canvas_width,
-                 canvas_height, ball_radius, num_balls)
-while (True):
+
+# Prompt the user to enter the number of balls
+num_balls = 5
+
+# Initialize balls with random positions
+balls.initializing(canvas_width, canvas_height, ball_radius, num_balls)
+
+while True:
     turtle.clear()
     for i in range(num_balls):
-        ball.draw_circle(ball_color[i], ball_radius, xpos[i], ypos[i])
-        ball.move_circle(i, xpos, ypos, vx, vy, canvas_width,
-                         canvas_height, ball_radius)
+        balls.draw_circle(
+            balls.ball_color[i], ball_radius, balls.xpos[i], balls.ypos[i])
+        balls.move_circle(i, canvas_width, canvas_height, ball_radius)
     turtle.update()
 
-# hold the window; close it by clicking the window close 'x' mark
+# Hold the window; close it by clicking the window close 'x' mark
 turtle.done()
